50a34f07d159045ff70b802c5dcfea97367f166d
Alejo Zuleta <jhoitan266@gmail.com>
2024-05-03 08:44:41 -0500

    company options

--- User/storedProcedure/LoginUser.sql
+++ User/storedProcedure/LoginUser.sql
@@ -1,6 +1,6 @@
 DELIMITER //
 CREATE PROCEDURE LoginUser(IN p_email VARCHAR(60)) BEGIN
-SELECT id_user, password_user
+SELECT id_user, password_user, user_type
 FROM USER
 WHERE email_user = p_email;
 END //
--- company/storedProcedure/CreateCompany.sql
+++ company/storedProcedure/CreateCompany.sql
@@ -1,13 +1,14 @@
 -- Procedimiento para crear una empresa
 DELIMITER //
 CREATE PROCEDURE IF NOT EXISTS CreateCompany(
-    IN p_company_name VARCHAR(100),
+    IN p_company_name VARCHAR(40),
     IN p_company_description VARCHAR(255),
-    IN p_company_owner INT,
-    IN p_location VARCHAR(255)
+    IN p_company_address VARCHAR(255),
+    IN p_company_phone VARCHAR(11),
+    IN p_company_owner INT
 )
 BEGIN
-    INSERT INTO Company(company_name, company_description, company_owner, location)
-    VALUES (p_company_name, p_company_description, p_company_owner, p_location);
+    INSERT INTO COMPANY(company_name, company_description, company_address, company_phone, company_owner)
+    VALUES (p_company_name, p_company_description, p_company_address, p_company_phone, p_company_owner);
 END //
 DELIMITER ;
