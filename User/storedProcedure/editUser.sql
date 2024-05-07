1f6e4b735aa4e675e6105179b78371681bada933
Alejo Zuleta <jhoitan266@gmail.com>
2024-05-07 08:09:00 -0500

    update user

--- User/storedProcedure/editUser.sql
+++ User/storedProcedure/editUser.sql
@@ -5,8 +5,6 @@ CREATE PROCEDURE EditUser(
     IN _lastName_user VARCHAR(40),
     IN _country_user VARCHAR(40),
     IN _phone_user VARCHAR(10),
-    IN _email_user VARCHAR(60),
-    IN _password_user VARCHAR(100),
     IN _user_type ENUM('Usuario', 'Comerciante')
 )
 BEGIN
@@ -15,8 +13,6 @@ BEGIN
         lastName_user = _lastName_user,
         country_user = _country_user,
         phone_user = _phone_user,
-        email_user = _email_user,
-        password_user = _password_user,
         user_type = _user_type
     WHERE id_user = _id_user;
 END //
